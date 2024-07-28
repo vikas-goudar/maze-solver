173e3d436a06597080260e3c0577c7f13935262e
vikas-goudar <162786132+vikas-goudar@users.noreply.github.com>
2024-07-29 02:38:17 +0530

    Update node.cpp

--- src/maze_representation/node.cpp
+++ src/maze_representation/node.cpp
@@ -1,1 +1,41 @@
+#include <random>
+#include <vector>
+#include "node.h"
 
+using namespace std;
+
+
+Node::Node(int xCoord , int yCoord, int numNeighbours){
+  this->xCoord = xCoord;
+  this->yCoord = yCoord;
+  this->numNeighbours = numNeighbours;
+  connections = new Node*[numNeighbours]();
+  connectionsSize = 0; //number of connections
+}
+
+Node::~Node(){
+  delete[] connections;
+  /* if nodes are dynamically allocated, delete them also */
+}
+
+vector<int> Node::getCoord(){
+  return {xCoord , yCoord};
+}
+
+void Node::connectNode(Node* node , int direction , int bidi){
+  connections[direction] = node;
+  connectionsInt.push_back(direction);
+  connectionsSize++;
+  if (bidi == 1){
+    node->connectNode(this , (direction+2)%4 , 0);
+  }
+}
+
+Node* Node::getRandomNeighbour(){
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<> distrib(0,connectionsSize - 1);
+  int randomNumber = distrib(gen);
+
+  return connections[randomNumber];
+}

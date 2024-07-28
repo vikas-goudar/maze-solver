78177efdc9b8555eed28ce85295ac17de7e6b7d4
vikas-goudar <162786132+vikas-goudar@users.noreply.github.com>
2024-07-29 02:31:50 +0530

    Update node.h

--- src/maze_representation/node.h
+++ src/maze_representation/node.h
@@ -1,1 +1,26 @@
+#ifndef NODE_H
+#define NODE_H
 
+#include <vector>
+
+using namespace std;
+
+class Node{
+  private:
+    int xCoord;
+    int yCoord;
+    int numNeighbours;
+    // [North , West , South , East]
+    Node** connections; //array of pointer of neighbours
+    vector<int> connectionsInt; //vector of index of connected neighbours in connections
+    int connectionsSize; //number of connected neighbours
+  
+  public:
+    Node(int xCoord , int yCoord , int numNeighbours);
+    ~Node();
+    vector<int> getCoord();
+    void connectNode(Node* node , int direction , int bidi);
+    Node* getRandomNeighbour();
+};
+
+#endif

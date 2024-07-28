c983b72e6574f214c14c8c499b869d16013047b4
vikas-goudar <162786132+vikas-goudar@users.noreply.github.com>
2024-07-29 02:30:34 +0530

    Update maze.h

--- src/maze_representation/maze.h
+++ src/maze_representation/maze.h
@@ -1,1 +1,24 @@
+#ifndef MAZE_H
+#define MAZE_H
 
+#include "node.h"
+#include <vector>
+
+using namespace std;
+
+class Maze{
+  private:
+    int size;
+    Node** arrayNodes;
+    int numNeighbours;
+
+  public:
+    Maze();
+    Maze(int size , int numNeighbours);
+    ~Maze();
+    Node* getRandomNode();
+    Node* getNode(int xCoord , int yCoord);
+    void printNodes();
+};
+
+#endif

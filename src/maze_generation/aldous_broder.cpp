166f837c6545cd5d6bfafdce1676381c2a91ef44
VikasGoudar <avikashasappeared@gmail.com>
2024-07-31 23:18:42 +0530

    modified aldous_broder
    fixed path to maze.h

--- src/maze_generation/aldous_broder.cpp
+++ src/maze_generation/aldous_broder.cpp
@@ -1,4 +1,4 @@
-#include "maze.h"
+#include "./maze_representation/maze.h"
 
 AldousBroder::AldousBroder(Maze* maze){
   this->size = maze->size;
--- src/maze_generation/aldous_broder.h
+++ src/maze_generation/aldous_broder.h
@@ -1,7 +1,7 @@
 #ifndef ALDOUS_BRODER_H
 #define ALDOUS_BRODER_H
 
-#include "maze.h"
+#include "./maze_representation/maze.h"
 
 class AldousBroder{
   private:

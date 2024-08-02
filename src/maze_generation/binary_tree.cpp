e5f33b43e00f3e84eaf233c1ea2bcff9fab5be6f
vikas-goudar <162786132+vikas-goudar@users.noreply.github.com>
2024-08-02 11:53:28 +0530

    Update binary_tree.cpp

--- src/maze_generation/binary_tree.cpp
+++ src/maze_generation/binary_tree.cpp
@@ -20,12 +20,39 @@ BinaryTree::BinaryTree(MazeGenerator* mazeGenerator){
 }
 
 int BinaryTree::randomSouthOrEast(std::pair<int,int> node){
-  return distrib(gen);
+  int existsSouth = 1;
+  int existsEast = 1;
+
+  if ((node.second*size+node.first+1)%size==0){
+    existsEast=0;
+  }
+  if ((node.second*size+node.first)>=size*(size-1)){
+    existsSouth=0;
+  }
+  
+  if (!(existsSouth || existsEast)){
+    return -1;
+  }
+  elseif (!existsSouth){
+    return 0;
+  }
+  elseif (!existsEast){
+    return 1;
+  }
+  else{
+    return distrib(gen);
+  }
 }
 
 void BinaryTree::generateMaze(){
   std::pair<int,int> node = make_pair(0,0);
   for (int i = 0; i<size*size; i++){
-    
+    int dir = randomSouthOrEast(node);
+    if (dir == 0){
+  MazeGenerator->connectNodes(node,make_pair(node.first+1,node.second);
+    }
+    else{
+    MazeGenerator->connectNodes(node,make_pair(node.first,node.second+1);
+    }
   }
 }

2543a91db5a5aed456a9f011174cb948f8a302b4
vikas-goudar <162786132+vikas-goudar@users.noreply.github.com>
2024-08-02 12:03:58 +0530

    Update binaryTree.h

--- src/maze_generation/binaryTree.h
+++ src/maze_generation/binaryTree.h
@@ -1,12 +1,3 @@
-for each cell randomly add either a "north" or a "west" passage to an adjacent cell { don't go out of bounds }
-
-the reason it is called binary tree is because each node can have upto 2 children
-
-The algorithm creates a maze that has a diagonal bias { since each cell exists to the right or up making it easy to go 
-from the south-west node to the north-east node but vice versa is hard }
-The northern and eastern boundaries will be unbroken {nodes can't point out of the maze, therefore in the northern and eastern
-boundaries the nodes has passages in the same direction } 
-
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 

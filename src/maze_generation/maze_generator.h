0c24c8322734d030f0efe7395a0d6ce3256f300c
vikas-goudar <162786132+vikas-goudar@users.noreply.github.com>
2024-08-02 08:21:48 +0530

    Update maze_generator.h

--- src/maze_generation/maze_generator.h
+++ src/maze_generation/maze_generator.h
@@ -18,7 +18,7 @@ class MazeGenerator{
     void markVisited(std::pair<int,int> node);
     std::pair<int,int> getRandomNode();
     std::pair<int,int> getRandomConnection(std::pair<int,int> node);
-    void connectNodes(std::pair<int,int> currentNode,std::pair<int,int> randomNode,int bidi);
+    void connectNodes(std::pair<int,int> sourceNode,std::pair<int,int> destinationNode,int bidi);
 };
 
 #endif
